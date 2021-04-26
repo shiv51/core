7ac2414f1536a9fe4e63a1fe705f8eace1da2588
smettu <shivaraj.mettu@verizonmedia.com>
2021-04-26 13:47:53 +0530

    Bsf probelms

---
+++ src/core/probelms/tree/bfs/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,16 @@
+package core.probelms.tree.bfs;
+
+import core.tree.TreeNode;
+
+public class BinaryTreeLevelOrderTraversal {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void traverse(TreeNode head) {
+		
+		System.out.println(head.getValue());
+		
+	}
+}
---
+++ src/core/tree/TreeNode.java
@@ -0,0 +1,37 @@
+package core.tree;
+
+public class TreeNode {
+
+	int value;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int value){
+		this.value= value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public TreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+	
+}
