318779e7af139fcdd940ced2251c1a5e6232984e
shiv51 <shiva.john51@gmail.com>
2021-04-30 10:38:37 +0530

    Depth first probelems and a new problem on rightview of tree

---
+++ src/core/probelms/tree/bfs/RightViewOfBinaryTree.java
@@ -0,0 +1,40 @@
+package core.probelms.tree.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import core.tree.TreeNode;
+
+public class RightViewOfBinaryTree {
+
+	public static void main(String[] args) {
+		TreeNode one = new TreeNode(1);
+		TreeNode two = new TreeNode(2);
+		TreeNode three = new TreeNode(3);
+		TreeNode four = new TreeNode(4);
+		one.setLeft(two);
+		one.setRight(three);
+		two.setLeft(four);
+		rightView(one);
+	}
+	
+	public static void rightView(TreeNode root) {
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		q.add(root);
+		int h =1;
+		while(!q.isEmpty()) {
+			TreeNode temp = null;
+			for(int i=0;i<h;i++) {
+				temp = q.poll();
+				if(temp==null)
+					break;
+				q.add(temp.getLeft());
+				q.add(temp.getRight());
+			}
+			h=h*2;
+			if(temp!=null)
+				System.out.println(temp.getValue());
+			
+		}
+	}
+}
---
+++ src/core/probelms/tree/dfs/BinaryTreeAllPathToSum.java
@@ -0,0 +1,54 @@
+package core.probelms.tree.dfs;
+
+import java.util.Stack;
+
+import core.tree.TreeNode;
+
+public class BinaryTreeAllPathToSum {
+	public static void main(String[] args) {
+		TreeNode one = new TreeNode(1);
+		TreeNode two = new TreeNode(2);
+		TreeNode three = new TreeNode(3);
+		TreeNode four = new TreeNode(4);
+		TreeNode five = new TreeNode(5);
+		one.setLeft(two);
+		one.setRight(three);
+		two.setLeft(four);
+		two.setRight(five);
+		System.out.println(sum1(one,41));
+	}
+
+	public static void sum(TreeNode root) {
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		s.push(root);
+		while(!s.isEmpty()) {
+			int h = s.size();
+			for(int i=0;i<h;i++) {
+				TreeNode temp = s.pop();
+				System.out.println(temp.getValue());
+				if(temp.getRight()!=null)
+					s.push(temp.getRight());
+				if(temp.getLeft()!=null)
+					s.push(temp.getLeft());
+				
+			}
+		}
+	}
+	
+	public static boolean sum1(TreeNode root, int sum) {
+		
+		
+		if(root.getValue() == sum) {
+			return true;
+		}
+		else if(root.getLeft()==null && root.getRight()==null) {
+			return false;
+		}
+		else {
+			return sum1(root.getRight(),sum-root.getValue()) || sum1(root.getLeft(),sum-root.getValue());
+			
+		}
+		
+		
+	}
+}
