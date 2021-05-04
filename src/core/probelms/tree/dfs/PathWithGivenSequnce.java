61aa176919aab3916f69adfaaf58c4adc6b2d9da
shiv51 <shiva.john51@gmail.com>
2021-05-04 14:06:10 +0530

    Few additional problems related dfs

---
+++ src/core/probelms/tree/dfs/PathWithGivenSequnce.java
@@ -0,0 +1,53 @@
+package core.probelms.tree.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import core.tree.TreeNode;
+
+public class PathWithGivenSequnce {
+
+	public static boolean find(TreeNode root, List<Integer> list, List<Integer> output) {
+		
+		if(root!=null && root.getLeft()==null && root.getRight()==null) {
+			list.add(root.getValue());
+			if(list.containsAll(output)) {
+				return true;
+			}
+			else {
+				list.remove(new Integer(root.getValue()));
+			}
+		}
+		else {
+			list.add(root.getValue());
+			boolean x = false , y = false;
+			if(root.getLeft()!=null)
+				 x = find(root.getLeft(), list, output) ;
+			if(root.getRight()!=null)
+				 y =  find(root.getRight(), list, output);
+			list.remove(new Integer(root.getValue()));
+			return x||y;
+			
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		TreeNode one = new TreeNode(1);
+		TreeNode two = new TreeNode(2);
+		TreeNode three = new TreeNode(3);
+		TreeNode four = new TreeNode(4);
+		TreeNode five = new TreeNode(5);
+		one.setLeft(two);
+		one.setRight(three);
+		two.setLeft(four);
+		two.setRight(five);
+		three.setRight(four);
+		List<Integer> l = new ArrayList<Integer>();
+		List<Integer> output = new ArrayList<Integer>();
+		output.add(1);
+		output.add(3);
+		output.add(9);
+		System.out.println(find(one,l,output));
+	}
+}
---
+++ src/core/probelms/tree/dfs/SumOfPathNumbers.java
@@ -0,0 +1,56 @@
+package core.probelms.tree.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import core.tree.TreeNode;
+
+public class SumOfPathNumbers {
+
+	public static void main(String[] args) {
+		TreeNode one = new TreeNode(1);
+		TreeNode two = new TreeNode(2);
+		TreeNode three = new TreeNode(3);
+		TreeNode four = new TreeNode(4);
+		TreeNode five = new TreeNode(5);
+		one.setLeft(two);
+		one.setRight(three);
+		two.setLeft(four);
+		two.setRight(five);
+		three.setRight(four);
+		List<TreeNode> l = new ArrayList<TreeNode>();
+		System.out.println();
+		List<List<TreeNode>> output = new ArrayList<List<TreeNode>>();
+		sumOfPathNumbers(one,l,output);
+		System.out.println(output);
+		int total1 = 0;
+		for (List<TreeNode> i : output) { 
+			int total = 0;
+			for (TreeNode j : i) { 
+				total = 10*total + j.getValue();
+			}
+		    total1 = total+total1;
+		}
+		System.out.println(total1);
+		//System.out.println(ll);
+		
+	}
+	
+	public static List<List<TreeNode>> sumOfPathNumbers(TreeNode root, List<TreeNode> list, List<List<TreeNode>> output) {
+		
+		if(root!=null && root.getLeft()==null & root.getRight()==null) {
+			list.add(root);
+			output.add(new ArrayList<TreeNode>(list));
+			list.remove(root);
+		}
+		else {
+			list.add(root);
+			if(root.getLeft()!=null)
+			sumOfPathNumbers(root.getLeft(), list, output);
+			if(root.getRight()!=null)
+			sumOfPathNumbers(root.getRight(), list, output);
+			list.remove(root);
+		}
+		return output;
+	}
+}
