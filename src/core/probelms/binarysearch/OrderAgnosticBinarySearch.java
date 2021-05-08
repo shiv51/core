94fe2b2984c080876b14c325ad2c4ee5aa5462e1
shiv51 <shiva.john51@gmail.com>
2021-05-08 20:15:05 +0530

    Binary serach related problems

---
+++ src/core/probelms/binarysearch/OrderAgnosticBinarySearch.java
@@ -0,0 +1,43 @@
+package core.probelms.binarysearch;
+
+public class OrderAgnosticBinarySearch {
+
+	public static void main(String[] args) {
+		int[] input = {5,4,3,2,1};
+		int mid = input.length/2;
+		int low = 0;
+		int high = input.length-1;
+		int t = 3;
+		 boolean found = false;
+		 boolean asc = true;
+		 if(input[0]>input[1]) {
+			 asc = false;
+		 }
+		
+		while(low<=high && (high-low)>1) {
+			if(input[high] == t || input[low] == t) {
+				System.out.println("Found");
+				found = true;
+				break;
+			}
+			
+			if(input[mid]==t) {
+				System.out.println("Found");
+				found = true;
+				break;
+			}
+			else if(asc && t>input[mid]){
+				low = mid;
+				mid = (high+low)/2;
+			}
+			else {
+				high= mid;
+				mid = (high+low)/2;
+			}
+		}
+		if(!found)
+			System.out.println("Nope!");
+		
+		
+	}
+}
